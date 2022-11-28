e035edeb226977f3e3d2ef4ed117b461fb663126
victor <victormgbeahurike82@gmail.com>
2022-11-28 23:11:39 +0100

    update

--- routes/email.js
+++ routes/email.js
@@ -1,4 +1,19 @@
 const router = require('express').Router()
 
+const nodemailer = require('nodemailer')
+require('dotenv/config')
+
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.AUTH_EMAIL,
+        pass: process.env.AUTH_PASS
+    },
+    tls: {
+        rejectUnauthorized:false
+    }
+})
+
 
 module.exports = router
