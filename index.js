c19e7d3781eb639c963dbc455d466cb89f8ecdbb
victor <victormgbeahurike82@gmail.com>
2022-11-28 22:32:43 +0100

    update

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const email = require('./routes/email')
+
+require('dotenv/config')
+
+app.use(express.static(__dirname + '/public'))
+app.set("view engine", "ejs");
+app.use('/', email)
+app.get('/', (req, res)=>{
+    res.render('home portfolio')
+})
+
+app.get('/about', (req, res)=>{
+    res.render('about us portfolio')
+})
+
+app.get('/services', (req, res)=>{
+    res.render('services')
+})
+
+app.get('/contact', (req, res)=>{
+    res.render('contact us portfolio')
+})
+
+const Port = process.env.PORT || 3008
+app.listen(Port, ()=>{
+    console.log(`${Port}`)
+})
